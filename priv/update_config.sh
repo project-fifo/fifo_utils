7b570d4c72a1eaf6dfc3a0cdc68a8df9353348c7
Heinz N. Gies <heinz@licenser.net>
2015-01-09 11:00:34 +0100

    Fixed sed and grep

--- priv/update_config.sh
+++ priv/update_config.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+sed=sed
+grep=grep
+if [ -f /opt/local/bin/sed ]
+then
+  sed=/opt/local/bin/sed
+fi
+if [ 0f /opt/local/bin/grep ]
+then
+  grep=/opt/local/bin/grep
+fi
+
 example=$1
 old=$2
 get() {
     var=$2
     conf=$1
 
-    res=$(grep "^\\s*${var}\\s*=" $conf | sed "s/.*${var}[ ]*=[ ]*//")
+    res=$($grep "^\\s*${var}\\s*=" $conf | $sed "s/.*${var}[ ]*=[ ]*//")
     if [ -z "${res}" ]
     then
         exit 1
@@ -17,20 +28,20 @@ get() {
 
 while read line
 do
-    if echo "${line}" | grep -v '^#' | grep -v '^$' > /dev/null
+    if echo "${line}" | $grep -v '^#' | $grep -v '^$' > /dev/null
     then
-        key=$(echo "${line}" | sed 's/[ ]*=.*//')
+        key=$(echo "${line}" | $sed 's/[ ]*=.*//')
         if val=$(get ${old} ${key})
         then
             echo "${key} = ${val}"
         else
             echo "${line}"
         fi
-    elif echo "${line}" | grep '^#\+\s*.\+=.\+' > /dev/null
+    elif echo "${line}" | $grep '^#\+\s*.\+=.\+' > /dev/null
     then
          ## If the line looks like a commented value try to find that
          ## git puvalue in the old config to see if we need to uncomment it
-         key=$(echo "${line}" | sed 's/[ ]*=.*$//' | sed 's/^#*[ ]*//')
+         key=$(echo "${line}" | $sed 's/[ ]*=.*$//' | $sed 's/^#*[ ]*//')
          if val=$(get ${old} ${key})
          then
              echo "${key} = ${val}"
