74500c767700b4a612bcaf8468458cf7ecbdd315
Heinz N. Gies <heinz@licenser.net>
2015-03-06 17:49:16 +0100

    added pkg.mk file

--- priv/pkg.mk
+++ priv/pkg.mk
@@ -1,38 +1,44 @@
-BASE_DIR ?= /opt/local
+
+TARGET_DIR ?= /opt/local/$(COMPONENT)
 FILE ?= $(COMPONENT)-$(VERSION)$(SUFFIX)
 BLOCK_SIZE ?= 65536
+STAGE_DIR ?= deploy
+TMP_DIR ?= tmp
+PKG_CATEGORY ?= fifo
+PKG_HOMEPAGE ?= https://project-fifo.net
 
 package_list:
 	-rm packlist
 	for dep in $(DEPS); do echo "@pkgdep $$dep" >> packlist; done
-	(cd ../../; find LICENSE* -type f | sort) >> packlist
-	(cd ../../; find config -type f | sort) >> packlist
-	(cd ../../; find dist -type f | sort) >> packlist
+	(cd deploy; find * -type f | sort) >> packlist
 
 build_info:
 	pkg_info -X pkg_install | egrep '^(MACHINE_ARCH|OPSYS|OS_VERSION|PKGTOOLS_VERSION)' >build-info
-	echo "CATEGORIES=fifo" >>build-info
-	echo "HOMEPAGE=http://project-fifo.net/" >>build-info
+	echo "CATEGORIES=$(PKG_CATEGORY)" >>build-info
+	echo "HOMEPAGE=$(PKG_HOMEPAGE)" >>build-info
 
-clean:
-	-rm *.tgz build-info packlist
+clean-pkg:
+	-rm -r tmp *.tgz build-info packlist
 
-tmp/$(FILE).tgz: package_list build_info
-	-rm -r tmp
-	mkdir tmp
-	pkg_create -i install.sh -k deinstall.sh -D displayfile -B build-info -c comment -d description -f packlist -I $(BASE_DIR)/$(COMPONENT) -p ../../ -U tmp/$(FILE).tgz
+
+$(TMP_DIR)/$(FILE).tgz: package_list build_info
+	-rm -r $(TMP_DIR)
+	mkdir $(TMP_DIR)
+	pkg_create -i install.sh -k deinstall.sh -D displayfile -B build-info -c comment -d description -f packlist -I $(TARGET_DIR) -p ../../ -U $(TMP_DIR)/$(FILE).tgz
 
 ifdef GPG_KEY
-$(FILE).tgz: tmp/$(FILE).tgz +PKG_HASH +PKG_GPG_SIGNATURE
+$(FILE).tgz: $(TMP_DIR)/$(FILE).tgz +PKG_HASH +PKG_GPG_SIGNATURE
 	-rm $(FILE).tgz
-	gar r $(FILE).tgz +PKG_HASH +PKG_GPG_SIGNATURE tmp/$(FILE).tgz
+	gar r $(FILE).tgz +PKG_HASH +PKG_GPG_SIGNATURE $(TMP_DIR)/$(FILE).tgz
+	rm -r $(TMP_DIR)
 else
-$(FILE).tgz: tmp/$(FILE).tgz
-	cp tmp/$(FILE).tgz $(FILE).tgz
+$(FILE).tgz: $(TMP_DIR)/$(FILE).tgz
+	cp $(TMP_DIR)/$(FILE).tgz $(FILE).tgz
+	rm -r $(TMP_DIR)
 endif
 
 
-+PKG_HASH: tmp/$(FILE).tgz
++PKG_HASH: $(TMP_DIR)/$(FILE).tgz
 	split -b $(BLOCK_SIZE) tmp/$(FILE).tgz tmp/split
 	echo "pkgsrc signature\n\nversion: 1\npkgname: $(FILE)" > +PKG_HASH
 	echo "algorithm: SHA512\nblock size: $(BLOCK_SIZE)" >> +PKG_HASH
