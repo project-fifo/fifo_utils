2d6eb2853b3f10338c2e8742ea39f1053498a1ba
Heinz N. Gies <heinz@licenser.net>
2017-10-01 02:46:27 +0200

    Fix suffix for pkgng

--- priv/pkgng.mk
+++ priv/pkgng.mk
@@ -1,7 +1,5 @@
 TARGET_DIR ?=/usr/local/lib
 RC_DIR ?=/usr/local/etc/rc.d
-FILE ?=$(COMPONENT)-$(VERSION)$(SUFFIX)
-BLOCK_SIZE ?=65536
 STAGE_DIR ?=deploy
 TMP_DIR ?=tmp
 PKG_CATEGORY ?=fifo
@@ -17,22 +15,14 @@ $(STAGE_DIR)/plist:
 	-rm $(STAGE_DIR)/plist || true
 	(cd $(STAGE_DIR); find * -type f | sort | grep -v '^+' | grep -v '^plist$$') >> $(STAGE_DIR)/plist
 
-clean-pkg:
-
 $(STAGE_DIR)/+MANIFEST: +MANIFEST.in
-	sed -e 's/__VNS__/${VERSION}/' +MANIFEST.in > $(STAGE_DIR)/+MANIFEST
+	sed -e 's/__VNS__/${VERSION}${SUFFIX}/' +MANIFEST.in > $(STAGE_DIR)/+MANIFEST
 	( echo 'deps:  { '; \
 		for dep in ${DEPS}; do \
 		pkg query --glob "	\"%n\" : { \"origin\" : \"%o\", \"version\" : \"%v\" }," "$$dep"; \
 		done ; \
 		echo '}' ) >> $(STAGE_DIR)/+MANIFEST
 
-tmp/$(FILE).tgz: dep_list package_list +MANIFEST
-	-rm -r tmp
-	mkdir tmp
-	pkg_create -i install.sh -k deinstall.sh -D displayfile -B build-info -c comment -d description -f packlist -I $(TARGET_DIR) -p $(STAGE_DIR) -U tmp/$(FILE).tgz
+clean-pkg:
 
 clean: clean-pkg
-	-rm -r tmp build-info packlist
-	-rm -r $(STAGE_DIR)
-	-rm *.tgz
